0b2d8004ae5c2e41b70c80faa1b0a812c92fcf0c
kwenealete <kwenait@yahoo.ca>
2024-08-26 04:10:05 +0200

    Banner component added. The banner section displays a welcome note,  brief history about myself in, a link for possible connections and various job functions/title I pride myself in.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/Navigate";
+import Banner from "./components/Banner";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }

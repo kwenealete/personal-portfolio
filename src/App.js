e7af3321ef606bb4e110e26d48612208c33bb253
kwenealete <kwenait@yahoo.ca>
2024-08-26 04:56:59 +0200

    Skills component. A display of my skill toolset.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import NavBar from "./components/Navigate";
 import Banner from "./components/Banner";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
